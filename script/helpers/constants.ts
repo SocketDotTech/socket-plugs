73d73954111013a8116aa028e9231adb04a28e88
arthcp <arth.c.patel@gmail.com>
2023-08-18 20:38:42 +0530

    feat: adjust limits

--- script/helpers/constants.ts
+++ script/helpers/constants.ts
@@ -64,7 +64,7 @@ export const totalSupply = utils.parseUnits(
 );
 
 export const FAST_MAX_LIMIT = utils.parseUnits(
-  "100000",
+  "10000",
   tokenDecimals[tokenToBridge]
 );
 export const SLOW_MAX_LIMIT = utils.parseUnits(
@@ -73,7 +73,7 @@ export const SLOW_MAX_LIMIT = utils.parseUnits(
 );
 
 export const FAST_RATE = utils.parseUnits(
-  "1.1574",
+  "0.11574",
   tokenDecimals[tokenToBridge]
 );
 export const SLOW_RATE = utils.parseUnits("2", tokenDecimals[tokenToBridge]);
