1216f6a6a33925635059b78926103a75cc5ecbdc
Aniket Sharma <aniket965.as@gmail.com>
2023-08-21 12:07:40 +0400

    feat: add create3

--- src/DeployMintableTokenStack.sol
+++ src/DeployMintableTokenStack.sol
@@ -1,16 +1,25 @@
 pragma solidity ^0.8.13;
 import "./MintableToken.sol";
 import "./ExchangeRate.sol";
-import "./Controller.sol";
+import "./ControllerV2.sol";
 import "./ConnectorPlug.sol";
 import "./interfaces/IPlug.sol";
 import "./interfaces/ISocket.sol";
 import "solmate/auth/Owned.sol";
+import "solmate/utils/CREATE3.sol";
+
+// TODO: discuss and remove 2step ownership
+// TODO: make sure create 3 is creating correct connection 
+// TODO: check if someone cant deploy a token with the same name and symbol and do harmfull things
+// FIXME: add natspec
+// TODO: gas optimization left
+// TODO: checks addition left
+
 struct LimitParams {
     uint256 maxLimit;
     uint256 ratePerSecond;
 }
-struct Data {
+struct DeploymentInfo {
     string tokenName;
     string tokenSymbol;
     address owner;
@@ -22,11 +31,12 @@ struct Data {
 }
 struct DeployToChains {
     uint256[] gasLimits;
-    Data data;
+    uint256 initialSupply;
+    DeploymentInfo data;
 }
 
-contract DeployMintableTokenStack is IPlug, Owned{
-    event TokenDeployed(address token);
+contract DeployMintableTokenStack is IPlug, Owned {
+    event TokenDeployed(address token, address owner);
     event ControllerDeployed(address controller, address token);
     event ConnectorDeployedAndConnected(
         address connector,
@@ -40,18 +50,21 @@ contract DeployMintableTokenStack is IPlug, Owned{
 
     error NotSocket();
 
-    constructor(address _socket, uint32 _chainSlug, ExchangeRate _exchangeRate) Owned(msg.sender) {
+    constructor(
+        address _socket,
+        uint32 _chainSlug,
+        ExchangeRate _exchangeRate
+    ) Owned(msg.sender) {
         socket = _socket;
         chainSlug = _chainSlug;
         exchangeRate = _exchangeRate;
     }
 
-
     function connect(
         address siblingPlug_,
         address switchboard_,
         uint32 siblingChainSlug_
-    ) external onlyOwner  {
+    ) external onlyOwner {
         ISocket(socket).connect(
             siblingChainSlug_,
             siblingPlug_,
@@ -60,46 +73,74 @@ contract DeployMintableTokenStack is IPlug, Owned{
         );
     }
 
-    function deploy(Data memory data) public {
-        Controller.UpdateLimitParams[]
-            memory updates = new Controller.UpdateLimitParams[](
+    function deploy(DeploymentInfo memory data, uint256 initialSupply) public {
+        ControllerV2.UpdateLimitParams[]
+            memory updates = new ControllerV2.UpdateLimitParams[](
                 data.limitParams.length
-            );
-
-        address token = address(
-            new MintableToken(
+        );
+        bytes32 salt = keccak256(
+            abi.encodePacked(
                 data.tokenName,
                 data.tokenSymbol,
                 data.tokenDecimals
             )
         );
+        address token = CREATE3.deploy(
+            salt,
+            abi.encodePacked(
+                type(MintableToken).creationCode,
+                abi.encode(data.tokenName, data.tokenSymbol, data.tokenDecimals)
+            ),
+            0
+        );
+        emit TokenDeployed(token, data.owner);
 
-        emit TokenDeployed(token);
-
-        Controller controller = new Controller(token, address(exchangeRate));
-        emit ControllerDeployed(address(controller), token);
+        bytes32 controllerSalt = keccak256(
+            abi.encodePacked(token, address(exchangeRate))
+        );
+        address controllerAddress = CREATE3.deploy(
+            controllerSalt,
+            abi.encodePacked(
+                type(ControllerV2).creationCode,
+                abi.encode(token, address(exchangeRate))
+            ),
+            0
+        );
+        ControllerV2 controller = ControllerV2(controllerAddress);
+        emit ControllerDeployed(controllerAddress, token);
 
         for (uint8 i = 0; i < data.chains.length; ) {
             if (data.chains[i] != chainSlug) {
-                ConnectorPlug connector = new ConnectorPlug(
-                    address(controller),
-                    socket,
-                    data.chains[i]
+                bytes32 connectorSalt = keccak256(
+                    abi.encodePacked(
+                        controllerAddress,
+                        socket,
+                        i 
+                    )
                 );
+                address connectorAddress = CREATE3.deploy(
+                    connectorSalt,
+                    abi.encodePacked(
+                        type(ConnectorPlug).creationCode,
+                        abi.encode(controllerAddress, socket, data.chains[i])
+                    ),
+                    0
+                );
+                ConnectorPlug connector = ConnectorPlug(connectorAddress);
                 connector.connect(
                     data.siblingConnectors[i],
                     data.switchboard[i]
                 );
                 emit ConnectorDeployedAndConnected(
-                    address(connector),
-                    address(controller),
+                    connectorAddress,
+                    controllerAddress,
                     data.chains[i]
                 );
 
                 // TODO: try to find out whether we can do better this in terms of gas savings
-                updates[i] = Controller.UpdateLimitParams({
+                updates[i] = ControllerV2.UpdateLimitParams({
                     isMint: true,
-                    connector: address(connector),
+                    connector: connectorAddress,
                     maxLimit: data.limitParams[i].maxLimit,
                     ratePerSecond: data.limitParams[i].ratePerSecond
                 });
@@ -112,18 +153,17 @@ contract DeployMintableTokenStack is IPlug, Owned{
             }
         }
 
-        
         controller.updateLimitParams(updates);
         controller.transferOwnership(data.owner);
-        //fixme: remove this just for testing
-        MintableToken(token).mint(data.owner, 1000000000000000000000000000);
-        MintableToken(token).transferOwnership(address(controller));
+
+        MintableToken(token).mint(data.owner, initialSupply);
+        MintableToken(token).transferOwnership(controllerAddress);
     }
 
     function deployMultiChain(DeployToChains calldata data) external payable {
         for (uint8 i = 0; i < data.data.chains.length; ) {
             if (data.data.chains[i] == chainSlug) {
-                deploy(data.data);
+                deploy(data.data, data.initialSupply);
             } else {
                 _outbound(
                     data.gasLimits[i],
@@ -143,8 +183,8 @@ contract DeployMintableTokenStack is IPlug, Owned{
         bytes calldata payload_
     ) external payable override {
         if (msg.sender != address(socket)) revert NotSocket();
-        Data memory data = abi.decode(payload_, (Data));
-        deploy(data);
+        DeploymentInfo memory data = abi.decode(payload_, (DeploymentInfo));
+        deploy(data, 0);
     }
 
     function _outbound(
