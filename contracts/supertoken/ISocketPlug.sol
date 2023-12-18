49dd84b4a8386d06be97da03d215cf1daf674403
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-12-19 01:49:03 +0530

    feat: interfaces

---
+++ contracts/supertoken/ISocketPlug.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.13;
+
+interface ISocketPlug {
+    function outbound(
+        uint32 siblingChainSlug_,
+        uint256 msgGasLimit_,
+        bytes memory payload_
+    ) external payable returns (bytes32 messageId_);
+
+    function getMinFees(
+        uint32 siblingChainSlug_,
+        uint256 msgGasLimit_
+    ) external view returns (uint256 totalFees);
+
+    function getMessageId(
+        uint32 siblingChainSlug_
+    ) external view returns (bytes32);
+}
