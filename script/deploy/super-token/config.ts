be297ef373dfeaca523edb222be38aae19160263
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-12-20 15:26:06 +0530

    feat: super token deployments

--- script/deploy/super-token/config.ts
+++ script/deploy/super-token/config.ts
@@ -2,17 +2,17 @@ import { socketOwner } from "../../helpers/constants";
 import { ChainSlug, IntegrationTypes } from "@socket.tech/dl-core";
 
 export const config = {
-  projectName: "TIMESWAP",
+  projectName: "supertoken",
   tokenName: "time",
   tokenSymbol: "TIME",
   tokenDecimal: 18,
   initialSupplyOwner: socketOwner,
   owner: socketOwner,
-  initialSupply: 0, //parseUnits("1000000", "ether"),
-  superTokenChains: [ChainSlug.OPTIMISM_GOERLI, ChainSlug.ARBITRUM_GOERLI],
+  initialSupply: 0,
+  superTokenChains: [ChainSlug.OPTIMISM, ChainSlug.ARBITRUM],
   vaultTokens: {
-    [ChainSlug.POLYGON_MUMBAI]: {
-      token: "0xB24d5ef4066Ba3eAF276313F6c476b82a5eDd780",
+    [ChainSlug.POLYGON_MAINNET]: {
+      token: "0x7Ae121F6c05057e23F4D7700e596c2a194694529",
     },
   },
   integrationType: IntegrationTypes.fast,
