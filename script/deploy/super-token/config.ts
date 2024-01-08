73bf9c8c2bf70e51ab957f2c44827b13c63c2859
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2024-01-08 23:03:05 +0530

    feat: look deployment

--- script/deploy/super-token/config.ts
+++ script/deploy/super-token/config.ts
@@ -3,27 +3,27 @@ import { socketOwner } from "../../helpers/constants";
 import { ChainSlug, IntegrationTypes } from "@socket.tech/dl-core";
 
 export const config = {
-  projectName: "supertoken",
-  tokenName: "time",
-  tokenSymbol: "TIME",
+  projectName: "looks",
+  tokenName: "looks",
+  tokenSymbol: "LOOKS",
   tokenDecimal: 18,
   initialSupplyOwner: socketOwner,
   owner: socketOwner,
   initialSupply: 0,
-  superTokenChains: [ChainSlug.OPTIMISM, ChainSlug.ARBITRUM],
+  superTokenChains: [ChainSlug.ARBITRUM_SEPOLIA, ChainSlug.OPTIMISM_SEPOLIA],
   vaultTokens: {
-    [ChainSlug.POLYGON_MAINNET]: {
-      token: "0x7Ae121F6c05057e23F4D7700e596c2a194694529",
+    [ChainSlug.SEPOLIA]: {
+      token: "0xa68c2CaA3D45fa6EBB95aA706c70f49D3356824E",
     },
   },
   integrationType: IntegrationTypes.fast,
-  depositLimit: "10000",
-  depositRate: "0.11574",
-  withdrawLimit: "10000",
-  withdrawRate: "0.11574",
+  depositLimit: "150000",
+  depositRate: "1.7361",
+  withdrawLimit: "150000",
+  withdrawRate: "1.7361",
 };
 
-export const srcChain = ChainSlug.POLYGON_MAINNET;
-export const dstChain = ChainSlug.ARBITRUM;
+export const srcChain = ChainSlug.ARBITRUM_SEPOLIA;
+export const dstChain = ChainSlug.SEPOLIA;
 export const gasLimit = 500_000;
-export const amount = utils.parseUnits("5", config.tokenDecimal);
+export const amount = utils.parseUnits("1", config.tokenDecimal);
