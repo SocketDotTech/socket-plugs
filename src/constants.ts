24950cfc5a20daaccf97277c81aed66b3b7cb219
Akash <akash.minato@gmail.com>
2024-04-03 21:03:10 +0530

    chore : readme

--- src/constants.ts
+++ src/constants.ts
@@ -34,15 +34,15 @@ export const tokenDecimals: { [key in Tokens]: number } = {
   [Tokens.WSTETH]: 18,
 };
 
-// export const ChainSlugToProject: { [chainSlug in ChainSlug]?: Project } = {
-//   [ChainSlug.AEVO]: Project.AEVO,
-//   [ChainSlug.AEVO_TESTNET]: Project.AEVO_TESTNET,
-//   [ChainSlug.LYRA_TESTNET]: Project.LYRA_TESTNET,
-//   [ChainSlug.LYRA]: Project.LYRA,
-//   [ChainSlug.SX_NETWORK_TESTNET]: Project.SX_NETWORK_TESTNET,
-//   // [ChainSlug.OPTIMISM_SEPOLIA]: Project.SOCKET_DEV,
-//   [ChainSlug.MODE_TESTNET]: Project.MODE_TESTNET,
-//   [ChainSlug.VICTION_TESTNET]: Project.VICTION_TESTNET,
-//   [ChainSlug.MODE]: Project.MODE,
-//   [ChainSlug.ANCIENT8_TESTNET2]: Project.ANCIENT8_TESTNET2,
-// };
+export const ChainSlugToProject: { [chainSlug in ChainSlug]?: Project } = {
+  [ChainSlug.AEVO]: Project.AEVO,
+  [ChainSlug.AEVO_TESTNET]: Project.AEVO_TESTNET,
+  [ChainSlug.LYRA_TESTNET]: Project.LYRA_TESTNET,
+  [ChainSlug.LYRA]: Project.LYRA,
+  [ChainSlug.SX_NETWORK_TESTNET]: Project.SX_NETWORK_TESTNET,
+  // [ChainSlug.OPTIMISM_SEPOLIA]: Project.SOCKET_DEV,
+  [ChainSlug.MODE_TESTNET]: Project.MODE_TESTNET,
+  [ChainSlug.VICTION_TESTNET]: Project.VICTION_TESTNET,
+  [ChainSlug.MODE]: Project.MODE,
+  [ChainSlug.ANCIENT8_TESTNET2]: Project.ANCIENT8_TESTNET2,
+};
