625a2843d0a45a5e72fac6da10ceaeefba786a31
Akash <akash.minato@gmail.com>
2024-05-01 17:01:59 +0400

    chore : lint

--- script/setup/setupConfig.ts
+++ script/setup/setupConfig.ts
@@ -6,40 +6,40 @@ import { editProject } from "./editProject";
 config();
 export async function setupConfigs() {
   // while (1) {
-    const actionInfo = await prompts([
-      {
-        name: "action",
-        type: "select",
-        choices: [
-          {
-            title: "Add a new Project",
-            value: "addProject",
-          },
-          {
-            title: "Edit an existing Project",
-            value: "editProject",
-          },
-          {
-            title: "Add a new Token",
-            value: "addToken",
-          },
-          {
-            title: "Exit",
-            value: "exit",
-          },
-        ],
-        message: "Select an action to perform",
-      },
-    ]);
+  const actionInfo = await prompts([
+    {
+      name: "action",
+      type: "select",
+      choices: [
+        {
+          title: "Add a new Project",
+          value: "addProject",
+        },
+        {
+          title: "Edit an existing Project",
+          value: "editProject",
+        },
+        {
+          title: "Add a new Token",
+          value: "addToken",
+        },
+        {
+          title: "Exit",
+          value: "exit",
+        },
+      ],
+      message: "Select an action to perform",
+    },
+  ]);
 
-    if (actionInfo.action === "addProject") {
-      await addProject();
-    } else if (actionInfo.action === "editProject") {
-      await editProject();
-    } else if (actionInfo.action === "addToken") {
-      await addNewToken();
-    } else if (actionInfo.action === "exit") {
-      process.exit(0);
-    }
+  if (actionInfo.action === "addProject") {
+    await addProject();
+  } else if (actionInfo.action === "editProject") {
+    await editProject();
+  } else if (actionInfo.action === "addToken") {
+    await addNewToken();
+  } else if (actionInfo.action === "exit") {
+    process.exit(0);
+  }
   // }
 }
