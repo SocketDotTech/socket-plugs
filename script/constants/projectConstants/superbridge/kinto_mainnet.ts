f9569d37803b3268d83698b9a4061b2b9c9b3ab0
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-05-17 19:59:36 +0100

    chore: add kinto setup file

---
+++ script/constants/projectConstants/superbridge/kinto_mainnet.ts
@@ -0,0 +1,45 @@
+import {
+  ChainSlug,
+  DeploymentMode,
+  IntegrationTypes,
+} from "@socket.tech/dl-core";
+import { Hooks, ProjectConstants } from "../../../../src";
+import { Tokens } from "../../../../src/enums";
+
+export const pc: ProjectConstants = {
+  [DeploymentMode.PROD]: {
+    [Tokens.DAI]: {
+      vaultChains: [ChainSlug.BASE],
+      controllerChains: [ChainSlug.KINTO],
+      hook: {
+        hookType: Hooks.KINTO_HOOK,
+        limitsAndPoolId: {
+          [ChainSlug.MAINNET]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "1000000",
+              receivingLimit: "1000000",
+            },
+          },
+          [ChainSlug.KINTO]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "1000000",
+              receivingLimit: "1000000",
+            },
+          },
+          [ChainSlug.BASE]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "1000000",
+              receivingLimit: "1000000",
+            },
+          },
+          [ChainSlug.ARBITRUM]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "1000000",
+              receivingLimit: "1000000",
+            },
+          },
+        },
+      },
+    },
+  },
+};
